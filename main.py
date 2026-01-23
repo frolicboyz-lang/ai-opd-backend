11b256fd5ba45b8ef75dd61a215913cf2f5de7e1
frolicboyz-lang <frolicboyz@gmail.com>
2026-01-23 12:09:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,7 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
 
 app = FastAPI()
-
-class PatientRequest(BaseModel):
-    message: str
 
 @app.get("/")
 def root():
     return {"message": "AI OPD running!"}
-
-@app.post("/chat")
-def chat(request: PatientRequest):
-    return {
-        "response": f"You said: {request.message}"
-    }
-        return {"error": str(e)}
