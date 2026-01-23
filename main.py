677b60b4dc8c85b3d9f8960e17acc92944acaed4
frolicboyz-lang <frolicboyz@gmail.com>
2026-01-23 20:40:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,18 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"message": "AI OPD running!"}
+from fastapi import UploadFile, File
+import shutil
+import os
+
+@app.post("/voice")
+async def voice_opd(audio: UploadFile = File(...)):
+    file_path = f"temp_{audio.filename}"
+
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(audio.file, buffer)
+
+    return {
+        "message": "Voice received successfully",
+        "filename": audio.filename
+    }
